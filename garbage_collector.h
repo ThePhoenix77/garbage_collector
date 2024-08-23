1a03e266b75c07bb8f624dfa921fd7670eca9dea
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-08-23 11:24:30 +0100

    Create garbage_collector.h

---
+++ garbage_collector.h
@@ -0,0 +1,21 @@
+#ifndef GARBAGE_COLLECTOR_H
+#define GARBAGE_COLLECTOR_H
+
+#include <stdlib.h>
+
+typedef struct block
+{
+    void *ptr;
+    struct block *next;
+} block;
+
+typedef struct memory_context
+{
+    block *head; 
+} memory_context;
+
+void *my_malloc(memory_context *context, size_t size);
+void my_free(memory_context *context, void *ptr);
+void free_all(memory_context *context);
+
+#endi
